de070ac115ab8c0d3fe6a0dceb262a55881decb8
Gopikrishna V.M <15769003+GKVM@users.noreply.github.com>
2017-12-12 21:19:19 +0530

    Changes TLD

--- js/set-it-up.js
+++ js/set-it-up.js
@@ -3,7 +3,7 @@ const applicationSelectionElement = document.getElementById("selected-apps");
 const valText = document.getElementById("input-apps");
 const runCommandBox = document.getElementById("run-command-box");
 let selectedApps = [];
-let fileName = "setthisup-default.sh";
+let fileName = "setup-default.sh";
 
 function getFile() {
     let script = "#!/bin/bash\n\n";
@@ -32,7 +32,7 @@ function getFile() {
             .concat(postUpdateCommand[i].post_update);
     }
 
-    fileName = `setthisup-${Number.parseInt(Math.random() * 10000)}.sh`;
+    fileName = `setup-${Number.parseInt(Math.random() * 10000)}.sh`;
     runCommandBox.textContent = "sh ~/Downloads/" + fileName;
 
     let element = document.createElement('a');
