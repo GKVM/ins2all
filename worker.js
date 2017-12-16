c6d17eb5119f8a49399a9b667d78fdbc99557a32
Gopikrishna V.M <15769003+GKVM@users.noreply.github.com>
2017-12-16 21:36:25 +0530

    Updates worker.js

--- worker.js
+++ worker.js
@@ -21,20 +21,6 @@ self.addEventListener('fetch', function(event) {
     event.respondWith(
         caches.match(event.request)
             .then(function(response) {
-                    // Cache hit - return response
-                    if (response) {
-                        return response;
-                    }
-                    return fetch(event.request);
-                }
-            )
-    );
-});
-
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request)
-            .then(function(response) {
                 // Cache hit - return response
                 if (response) {
                     return response;
